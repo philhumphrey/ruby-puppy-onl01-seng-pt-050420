1646697cd30f6d5fba69cc21a9abcac7743400a4
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-25 02:27:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+class Dog
+
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+   @@all << self
+  # @@dog_name
+  # end
+  end
+  
+  def self.all
+    @@all
+  end
+    
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all(name)
+    @@all.each do |name|
+     puts name.name
+    end
+  end
+end
+
+
+
+# def dog_name.print_all
+#   end
+# end
